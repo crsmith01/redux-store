ddde336bbd44dd7b9ae89b6ef928cbcb0bb36d6a
Catherine Smith <catsmith24601@gmail.com>
2021-07-28 09:44:10 -0400

    installed dependencies, removed extra spaces at bottom of files

--- client/src/App.js
+++ client/src/App.js
@@ -18,6 +18,11 @@ import { StoreProvider } from './utils/GlobalState';
 import Success from './pages/Success';
 import OrderHistory from './pages/OrderHistory';
 
+import { StoreProvider } from './utils/GlobalState';
+// import provider
+import { Provider } from 'react-redux';
+
+
 const httpLink = createHttpLink({
   uri: '/graphql',
 });
@@ -42,7 +47,7 @@ function App() {
     <ApolloProvider client={client}>
       <Router>
         <div>
-          <StoreProvider>
+          <StoreProvider store={store}>
             <Nav />
             <Switch>
               <Route exact path="/" component={Home} />
